31a323b09e01348089a2dc37493ac0d2c319d8c3
Tom <abaotom14@gmail.com>
2025-01-20 16:47:06 +0800

    Add distance filter

--- ride_app/filters.py
+++ ride_app/filters.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django_filters import rest_framework as filters
 
 from ride_app.models import Ride
@@ -5,6 +7,7 @@ from ride_app.models import Ride
 
 class RideFilter(filters.FilterSet):
     rider_email = filters.CharFilter(field_name="id_rider__email", lookup_expr="exact")
+    distance = filters.CharFilter(method="filter_distance")
     ordering = filters.OrderingFilter(
         fields=(
             ("pickup_time", "pickup_time"),
@@ -16,4 +19,9 @@ class RideFilter(filters.FilterSet):
         fields = (
             "status",
         )
+
+
+    def filter_distance(self, queryset, name, value):
+        position = [Decimal(v) for v in value.split(",")]
+        return queryset
 
